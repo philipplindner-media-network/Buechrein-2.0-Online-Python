ea23a3de2f80aa5cdb477bebe4879031465b06ca
Philipp Lindner Media & Network <post@philipp-lindner.de>
2025-08-18 16:25:28 +0200

    Update start.sh
    Fehler beseitigung

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-echo "Virtuelle Umgebung erstellen..."
-python3 -m venv venv
+# Wechsle in das Verzeichnis, in dem das Skript liegt, um relative Pfade zu ermöglichen
+cd "$(dirname "$0")"
 
-echo "Abhängigkeiten installieren..."
-source venv/bin/activate
+echo "Virtuelle Umgebung wird überprüft/erstellt..."
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+echo "Abhängigkeiten werden installiert..."
+. venv/bin/activate
 pip install -r requirements.txt
 
-echo "Skript starten..."
-python main.py
+echo "Skript wird gestartet..."
+python3 main.py
 
-deactivate
+# Die virtuelle Umgebung wird am Ende des Skripts automatisch deaktiviert.
